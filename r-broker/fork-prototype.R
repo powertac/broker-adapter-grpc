c2012c5d97bca2df071f7151e59a727771ffc234
John Collins <jcollins@cs.umn.edu>
2016-06-13 22:19:41 -0500

    improve prototype

--- r-broker/fork-prototype.R
+++ r-broker/fork-prototype.R
@@ -1,15 +1,24 @@
-runBroker <- function() {
+runBroker <- function(input, output) {
+  setwd("..")
+  infile <- file(input, "r")
+  outfile <- file(output, "w")
   pr <- parallel:::mcfork()
   if (inherits(pr, "masterProcess")) {
     # this is the child process
-    setwd("..")
-    system2("mvn", args="exec:exec")
+    system2("mvn", args="exec:exec -Dexec.args='--prop samplebroker.r.charStreamAdapter.inputFilename:broker-input --prop samplebroker.r.charStreamAdapter.outputFilename:broker-output'")
     parallel:::mcexit("child exit")
   }
   # parent process
-  v <- parallel:::readChild(pr)
-  print("Parent")
-  print(v)
+  done = FALSE
+  while (!done) {
+    line <- readlines(infile, n=1)
+    # process lines, send messages
+    print(line)
+    if (!is.na(charmatch("<ts-done", c(line)))) {
+      # process input here
+      writeLines(c("continue"), outfile)
+    }
+  }
 }
 
-runBroker()
+runBroker("broker-output", "broker-input")
